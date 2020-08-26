aa7bdd3ca39ddcdf6f35fff88b35ecedd22efba3
AndyT97 <andy.ta991@gmail.com>
2020-08-25 21:59:45 -0400

    Added some snippest to make worm sprite, used GetSpriteCollision to make worm disappear

--- Crab.cpp
+++ Crab.cpp
@@ -8,16 +8,22 @@ app App;
 
 float const app::PLAYER_SPEED = 200.0f;
 
+
 void app::Begin(void)
 {
+
 	agk::SetVirtualResolution (1024, 768);
+	//
 	background = agk::LoadSprite("media/beach.jpg");
+	//png guru
 	crab = agk::LoadSprite("media/crab.png");
-	worm = agk::LoadImage("media/worm.png");
+	worm = agk::LoadSprite("media/worm.png");
 
 	agk::SetSyncRate(60,0);
 	agk::SetScissor(0,0,0,0);
 
+	//TODO change this to random positions later
+	agk::SetSpritePosition(worm,700, 500);
 	player_x = agk::GetVirtualWidth() / 2.0f;
 	player_y = agk::GetVirtualHeight() / 2.0f;
 
@@ -25,6 +31,11 @@ void app::Begin(void)
 
 int app::Loop (void)
 {
+	srand(time(NULL));
+	//TODO pick better numbers
+	int newXpos = rand() % 900 + 100;
+	int newYpos = rand() % 500 + 200;
+
 	float const move_this_frame = PLAYER_SPEED * agk::GetFrameTime();
 	if (agk::GetRawKeyState(AGK_KEY_W)) {
 		player_y -= move_this_frame;
@@ -55,6 +66,11 @@ int app::Loop (void)
 	}
 	else if (player_y > max_y) {
 		player_y = max_y;
+	}
+
+	if (agk::GetSpriteCollision(crab, worm)) {
+		//agk::SetSpriteColor(worm, 255, 0, 0,255);
+		agk::SetSpritePosition(worm, newXpos, newYpos);
 	}
 
 	agk::SetSpritePositionByOffset(crab, player_x, player_y);
